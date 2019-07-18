08fe8ddf2d001a3696172879e1f6a757e790c5f6
Georgi Lalev <glalev@gmail.com>
2019-07-18 23:16:51 +0300

    Add a vuetify table test

---
+++ table-test/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+    <v-flex xs12 py-5 px-5>
+      <data-table :headers="headers" :data="items">
+        <template v-slot:nameHeader="{ data }">
+          <h1>{{ data.text }}</h1>
+        </template>
+        <template v-slot:name="{ data }">
+          <a href="https://google.com">{{ data.name }}</a>
+        </template>
+        <template v-slot:icon="{ data }">
+          <v-icon color="red">{{ data.icon }}</v-icon>
+        </template>
+      </data-table>
+    </v-flex>
+</template>
+
+<script>
+  import DataTable from './components/DataTable.vue';
+
+  export default {
+    name: 'App',
+    components: {
+      'data-table': DataTable,
+    },
+    data() {
+      return {
+        headers: [
+          { text: 'Name', value: 'name'},
+          { text: 'Age', value: 'age'},
+          { text: 'Sex', value: 'sex' },
+          { text: 'City', value: 'city'},
+          { text: 'Country', value: 'country' },
+          { text: 'Icon', value: 'icon' },
+        ],
+        items: [
+          { name: 'Georgi Lalev', age: 32, sex: 'male', city: 'Sofia', country: 'Bulgaria', icon: 'face', class: 'custom-class' },
+          { name: 'Philip Boune', age: 24, sex: 'male', city: 'Paris', country: 'France', icon: 'atm' },
+          { name: 'Anna Muller', age: 39, sex: 'female', city: 'Munich', country: 'Germany', icon: 'timer' },
+          { name: 'Natalia Petrova', age: 25, sex: 'female', city: 'Moskow', country: 'Russia', icon: 'category' },
+          { name: 'George Petrescu', age: 55, sex: 'male', city: 'Timisoara', country: 'Romania', icon: 'timer' },
+        ]
+      }
+    }
+  }
+</script>
---
+++ table-test/src/components/DataTable.vue
@@ -0,0 +1,36 @@
+<template>
+    <v-data-table
+            :headers="headers"
+            :items="data"
+            item-key="name"
+    >
+        <template slot="headerCell" slot-scope="{ header }">
+            <slot :name="`${header.value}Header`" v-bind:data="header">
+                <span>{{ header.text }}</span>
+            </slot>
+        </template>
+
+        <template v-slot:items="{ item }">
+            <tr :class="item.class">
+               <td v-for="(header, i) in headers" :key="i">
+                   <slot :name="header.value" v-bind:data="item">
+                       <span>{{ item[header.value] }}</span>
+                   </slot>
+               </td>
+            </tr>
+        </template>
+    </v-data-table>
+</template>
+
+<script>
+  export default {
+    name: "DataTable",
+    props: ['headers', 'data']
+  }
+</script>
+
+<style scoped>
+    .custom-class {
+        color: blue;
+    }
+</style>
